266a9fbef3114053d671119cb3b795f5196c0910
adriel alexander <adriel.alexs123@gmail.com>
2024-07-26 20:55:24 -0300

    updating header component with bootstrap lib

--- ilana-couto-interiores/src/index.js
+++ ilana-couto-interiores/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./bootstrap/custom-bootstrap.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/App.css";
 import reportWebVitals from "./reportWebVitals";
+import "./styles/header.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
