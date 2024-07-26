b6d8db4e6f3db7169c72afb891962d145c8dc343
adriel alexander <adriel.alexs123@gmail.com>
2024-07-25 22:36:15 -0300

    finishing components modeling

--- ilana-couto-interiores/src/App.js
+++ ilana-couto-interiores/src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import ChatButton from "./components/ChatButton";
-import "./styles/App.css";
+import "../src/styles/App.css";
 
 class App extends Component {
   render() {
--- ilana-couto-interiores/src/index.js
+++ ilana-couto-interiores/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/App.css"; //
 import App from "./App";
+import "./bootstrap/custom-bootstrap.scss";
+import "./styles/App.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
