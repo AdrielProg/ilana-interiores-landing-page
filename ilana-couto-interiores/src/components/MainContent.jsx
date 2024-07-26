27be5f4442cf4cdf671b509f20e8b9fa4c3f411e
adriel alexander <adriel.alexs123@gmail.com>
2024-07-25 22:36:15 -0300

    finishing components modeling

--- ilana-couto-interiores/src/components/MainContent.jsx
+++ ilana-couto-interiores/src/components/MainContent.jsx
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
 import "../styles/mainContent.css";
+import CarouselComponent from "./CarouselComponent";
+import profile from "../images/perfil.jpg";
+import logo from "../images/logo2.png";
 
 class MainContent extends Component {
   render() {
     return (
       <main className="mainContent">
         <div className="images">
-          <img src="image1.jpg" alt="Sala" className="image" />
-          <img src="image2.jpg" alt="Cozinha" className="image" />
+          <CarouselComponent></CarouselComponent>
+          <div className="description">
+            <picture>
+              <img className="profile" src={profile} alt="Foto-Projetista" />
+            </picture>
+            <div className="presentation">
+              <h2 className="about-me">Ilana Couto</h2>
+            </div>
+          </div>
         </div>
-        <div className="text"></div>
+        <div className="logo2">
+          <img src={logo} alt="" />
+        </div>
       </main>
     );
   }
